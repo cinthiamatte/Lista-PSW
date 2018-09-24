86d68146157dd43c4eb9e89b4b24e41b5794ef90
Cinthia Matte <cinthiamatte@gmail.com>
2018-09-23 23:41:29 -0300

    Final

--- Exercicio 5.2/src/udc/edu/psw/Soma.java
+++ Exercicio 5.2/src/udc/edu/psw/Soma.java
@@ -1,5 +1,32 @@
 package udc.edu.psw;
 
+import java.util.Scanner;
+
 public class Soma {
+	
+	private static Scanner scan;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		int vetor[] = new int [n];
+		int i;
+		
+		scan = new Scanner(System.in);
+		
+		for(i=0 ; i<n; i++) 
+		{
+			System.out.printf("Informe %2do. valor de %d: ", (i+1), n);
+			vetor[i] = scan.nextInt();
+		}
+		
+		  int soma = 0;
+
+	    for (i=0; i<n; i++) {
+	      soma = soma + vetor[i];
+	    }
+	    
+	    System.out.printf("Soma dos valores do Vetor: %d", soma);
+	}
 
 }
--- Exercicio 5.3/src/udc/edu/psw/Diferença.java
+++ Exercicio 5.3/src/udc/edu/psw/Diferença.java
@@ -1,5 +1,46 @@
 package udc.edu.psw;
 
-public class Diferença {
+import java.util.Scanner;
 
-}
+public class Diferença {
+	
+	private static Scanner scan;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		int vetor[] = new int [n];
+		int i;
+		
+		scan = new Scanner(System.in);
+		
+		for(i=0 ; i<n; i++) 
+		{
+			System.out.printf("Informe %2do. valor de %d: ", (i+1), n);
+			vetor[i] = scan.nextInt();
+		}
+		
+	    int menor = vetor[0]; 
+	    int maior = vetor[0];
+	    for (i=0; i<n; i++) {
+	 
+	      if (vetor[i] < menor)
+	         menor = vetor[i];
+	 
+	      if (vetor[i] > maior)
+	         maior = vetor[i];
+	    }
+
+	    System.out.printf("\n");
+	    for (i=0; i<n; i++) {
+	      if (vetor[i] == menor)
+	        System.out.printf("Valor[%d] = %2d <--- menor valor\n", i, vetor[i]);
+	      else if (vetor[i] == maior)
+	              System.out.printf("Valor[%d] = %2d <--- maior valor\n", i, vetor[i]);
+	           else System.out.printf("Valor[%d] = %2d\n", i, vetor[i]);
+	    }
+
+	  }
+	 
+	}
+
