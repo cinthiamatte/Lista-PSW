629e29b5b21dbb2ccb8ce7a567c57ce46aabbba2
Cinthia Matte <cinthiamatte@gmail.com>
2018-09-23 23:08:31 -0300

    Calculos e main

--- Exercicio 4.3/src/udc/edu/psw/ValordeX.java
+++ Exercicio 4.3/src/udc/edu/psw/ValordeX.java
@@ -1,5 +1,40 @@
 package udc.edu.psw;
+import java.util.Scanner;
 
 public class ValordeX {
+	
+	private static int x;
+	static Scanner scan = new Scanner(System.in);
+	
 
+	public static void Entrada() {
+		
+		
+		System.out.println("\nEntre com o valor de X");
+		x = scan.nextInt();
+		
+	}
+	
+	public static void Calculos() {
+		
+
+			if (x % 2 == 0) {
+				x = x / 2;
+				System.out.println("Valor de X:");
+				System.out.print(x);
+			}
+			else {
+				x = 2 * x / 3 + 1;
+				System.out.println("Valor de X:");
+				System.out.print(x);
+			}
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Entrada();
+		Calculos();
+		
+	}
 }
